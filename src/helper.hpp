6de0b552f5a90d310d1713f25a0cfa78bdbbb62e
Rígille S. B. Menezes <rigillesbmenezes@protonmail.com>
2022-03-10 08:03:31 -0300

    remove c++ files

--- src/helper.hpp
+++
@@ -1,5 +0,0 @@
-template<class... Ts> struct visitor : Ts... { using Ts::operator()...; };
-template<class... Ts> visitor(Ts...) -> visitor<Ts...>;
-
-template <class T>
-using Box = std::shared_ptr<T>;
--- src/parser/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "../parser.hpp"
-
-// one_empty
-// one_ok_tight
-// one_ok_loose
-// one_error_tight
-// one_error_loose
